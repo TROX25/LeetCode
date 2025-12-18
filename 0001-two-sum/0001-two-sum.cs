bb3b36e682ce65184bd38b352a57d1e6cdb676c8
Piotr Dziembowski <pimalogik@gmail.com>
2025-12-18 16:55:52 +0100

    0001-TWO-SUM Stats: Time: 30 ms (27.31%), Space: 48.2 MB (93.67%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) 
+    {
+        for (int i=0; i<nums.Length; i++)
+        {
+            int sub = target - nums[i];
+            for(int j = i+1; j<nums.Length ; j++)
+            {
+                if (sub - nums[j] == 0)
+                {
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return Array.Empty<int>();
+    }
+}

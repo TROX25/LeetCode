e124ad3db197e84d531f6d6f22d36a297f4ddb3e
Piotr Dziembowski <pimalogik@gmail.com>
2025-12-28 20:35:32 +0100

    0049-GROUP-ANAGRAMS Stats: Time: 19 ms (69.3%), Space: 68.3 MB (7.76%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cs
@@ -0,0 +1,43 @@
+public class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<string, List<string>> anagrams = new();
+
+        foreach (string word in strs)
+        {
+            // Tworze tablice do zliczania wystapien liter ktora wyglada tak: [a:0, b:0, c:0, ..., z:0]
+            int[] charCount = new int[26];
+
+            foreach (char c in word)
+            {
+                // Dzieki temu, dla kaÅ¼dej wystepujacej litery w stringu, zwiekszam odpowiednia pozycje w tablicy o 1
+                charCount[c - 'a'] += 1;
+                // dla slowa "tak", tablica bedzie wygladac tak: [a:1, b:0, c:0, ..., k:1, t:1, ..., z:0]
+            }
+
+            //WAÅ»NE: jako Å¼e suma ad i bc jest taka sama
+            //W zwiazku z tym z charCount musze zrobic stringa, zeby moc go uzyc jako klucz w slowniku
+
+            StringBuilder keyBuilder = new();
+            for (int i = 0; i < charCount.Length; i++)
+            {
+                keyBuilder.Append('#'); // separator
+                keyBuilder.Append(charCount[i]);
+                // Koncowo dostaje: #1#0#2#1...#3#0
+            }
+            string key = keyBuilder.ToString();
+
+            if (!anagrams.ContainsKey(key))
+            {
+                anagrams[key] = new List<string>();
+                //przyklad: "#1#0#0#0#0#0#0#0#0#0#1#0#0#0#0#0#0#0#0#1#0#0#0#0#0#0": []
+            }
+            anagrams[key].Add(word);
+            //przyklad: "#1#0#0#0#0#0#0#0#0#0#1#0#0#0#0#0#0#0#0#1#0#0#0#0#0#0": ["tak"]
+        
+        }
+        return new List<IList<string>>(anagrams.Values);
+
+    }
+}
--- README.md
+++ README.md
@@ -7,20 +7,24 @@ Collection of LeetCode questions
 |  |
 | ------- |
 | [0001-two-sum](https://github.com/TROX25/LeetCode/tree/master/0001-two-sum) |
+| [0049-group-anagrams](https://github.com/TROX25/LeetCode/tree/master/0049-group-anagrams) |
 | [0217-contains-duplicate](https://github.com/TROX25/LeetCode/tree/master/0217-contains-duplicate) |
 ## Hash Table
 |  |
 | ------- |
 | [0001-two-sum](https://github.com/TROX25/LeetCode/tree/master/0001-two-sum) |
+| [0049-group-anagrams](https://github.com/TROX25/LeetCode/tree/master/0049-group-anagrams) |
 | [0217-contains-duplicate](https://github.com/TROX25/LeetCode/tree/master/0217-contains-duplicate) |
 | [0242-valid-anagram](https://github.com/TROX25/LeetCode/tree/master/0242-valid-anagram) |
 ## Sorting
 |  |
 | ------- |
+| [0049-group-anagrams](https://github.com/TROX25/LeetCode/tree/master/0049-group-anagrams) |
 | [0217-contains-duplicate](https://github.com/TROX25/LeetCode/tree/master/0217-contains-duplicate) |
 | [0242-valid-anagram](https://github.com/TROX25/LeetCode/tree/master/0242-valid-anagram) |
 ## String
 |  |
 | ------- |
+| [0049-group-anagrams](https://github.com/TROX25/LeetCode/tree/master/0049-group-anagrams) |
 | [0242-valid-anagram](https://github.com/TROX25/LeetCode/tree/master/0242-valid-anagram) |
 <!---LeetCode Topics End-->

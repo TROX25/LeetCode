e993bb829baa038adc35f1125edfca887d6a364f
Piotr Dziembowski <pimalogik@gmail.com>
2026-04-17 13:40:34 +0200

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 18 ms (16.39%), Space: 52.9 MB (66.72%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cs
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cs
@@ -4,11 +4,11 @@ public class Solution
     {
         int[] mostFrequent = 
         nums
-            .GroupBy(x => x) 
-            .OrderByDescending(g => g.Count())
-            .Select(group => group.Key)
-            .Take(k)
-            .ToArray();
+            .GroupBy(x => x) // Grupuje takie same elementy razem (coÅ jak dict ale jest to IEnumerable<IGrouping<TKey, TElement>>)
+            .OrderByDescending(g => g.Count()) // sortuje grupy zaczynajac od tej ktÃ³ra ma najwiecej values w sobie 
+            .Select(group => group.Key) // wybiera same liczby. Poza .Key jest jeszcze .ToList() wtedy zwraca wszystkie wystapienia [[1,1,1], [2,2]] albo .Count() wtedy zwara iloÅÄ wystÄpien w kaÅ¼dej grupie [3,2,1] dla nums[1,1,1,2,2,3,3], 
+            .Take(k) // take wybiera pierwsze k elementÃ³w
+            .ToArray(); // zmienia z Ienumerable na Array. Moze byc jeszcze: ToList(), ToHashSet(), First() zwraca int, Last() zwraca int, Count() Liczba elementÃ³w, Sum() suma elementÃ³w, Min() minimum, Max() maximum
             
        return mostFrequent; 
     }
